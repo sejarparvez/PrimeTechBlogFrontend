896ad3ec3209545d0037fc544f63667f465544ea
sejar parvez <sejarparvez@yahoo.com>
2023-05-02 20:24:53 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar title="This is a title" paragraph="this is a paragraph" />
+    </div>
+  );
+}
+
+export default App;
